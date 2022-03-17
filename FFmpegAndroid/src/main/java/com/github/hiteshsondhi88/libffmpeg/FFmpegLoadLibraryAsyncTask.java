eae88ca6c06599c79a9524fbd345fa8a34afac4a
Igor Alyoshin <stawr88@bk.ru>
2022-03-18 02:07:29 +0300

    Update project #5

---
+++ FFmpegAndroid/src/main/java/com/github/hiteshsondhi88/libffmpeg/FFmpegLoadLibraryAsyncTask.java
@@ -0,0 +1,42 @@
+package com.github.hiteshsondhi88.libffmpeg;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.io.File;
+
+class FFmpegLoadLibraryAsyncTask extends AsyncTask<Void, Void, Boolean> {
+
+    private final String cpuArchNameFromAssets;
+    private final FFmpegLoadBinaryResponseHandler ffmpegLoadBinaryResponseHandler;
+    private final Context context;
+
+    FFmpegLoadLibraryAsyncTask(Context context, String cpuArchNameFromAssets, FFmpegLoadBinaryResponseHandler ffmpegLoadBinaryResponseHandler) {
+        this.context = context;
+        this.cpuArchNameFromAssets = cpuArchNameFromAssets;
+        this.ffmpegLoadBinaryResponseHandler = ffmpegLoadBinaryResponseHandler;
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        File ffmpegFile = new File(FileUtils.getFFmpeg(context));
+        return ffmpegFile.exists() && ffmpegFile.canExecute();
+    }
+
+    @Override
+    protected void onPostExecute(Boolean isSuccess) {
+        super.onPostExecute(isSuccess);
+        if (ffmpegLoadBinaryResponseHandler != null) {
+            if (isSuccess) {
+                ffmpegLoadBinaryResponseHandler.onSuccess();
+            } else {
+                ffmpegLoadBinaryResponseHandler.onFailure();
+            }
+            ffmpegLoadBinaryResponseHandler.onFinish();
+        }
+    }
+
+    private boolean isDeviceFFmpegVersionOld() {
+        return CpuArch.fromString(FileUtils.SHA1(FileUtils.getFFmpeg(context))).equals(CpuArch.NONE);
+    }
+}
